5a3253ef1146b1e26173a20a481f09818fb25194
ChangminYi <jerry9803@gmail.com>
2021-04-01 17:26:37 +0900

    gitigonre modified

---
+++ compiler-lexical-analyzer/compiler-lexical-analyzer/lexical_analyzer.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <fstream>
+#include "token.h"
+
+using namespace std;
+
+int main(int argc, char *argv[]){
+	cout << "hello world!" << endl;
+	cout << "this is test code";
+
+    return 0;
+}
---
+++ compiler-lexical-analyzer/compiler-lexical-analyzer/token.h
@@ -0,0 +1,19 @@
+#include <string>
+
+#ifndef TOKEN_H
+#define TOEKN_H
+
+enum TOKEN_TYPE{
+    TYPE,
+    NUMBER,
+    CHAR,
+    STRING,
+	BOOLEAN
+};
+
+class Token{
+    TOKEN_TYPE type;
+    std::string value;
+};
+
+#endif
