f8cca4df918a76c91a8889e76fa2fb722fa9b1e9
tjwjdgus12 <01048733438a@gmail.com>
2021-04-05 16:18:11 +0900

    token.h 토큰 추가, 출력연산 오버로딩

--- compiler-lexical-analyzer/compiler-lexical-analyzer/token.h
+++ compiler-lexical-analyzer/compiler-lexical-analyzer/token.h
@@ -3,17 +3,44 @@
 #ifndef TOKEN_H
 #define TOEKN_H
 
-enum TOKEN_TYPE{
+enum TOKEN_TYPE
+{
     TYPE,
-    NUMBER,
-    CHAR,
-    STRING,
-	BOOLEAN
+    NUMBER, CHAR, STRING, BOOLEAN,
+    ID,
+    IF, ELSE, WHILE, RETURN,
+    ASSIGN, COMPARE,
+    LPAREN, RPAREN,
+    LBRACE, RBRACE,
+    LBRACKET, RBRACKET,
+    COMMA, SEMICOLON,
+    WSPACE
+};
+
+const char *token_str[] = 
+{   
+    "TYPE",
+    "NUMBER", "CHAR", "STRING", "BOOLEAN",
+    "ID",
+    "IF", "ELSE", "WHILE", "RETURN",
+    "ASSIGN", "COMPARE",
+    "LPAREN", "RPAREN",
+    "LBRACE", "RBRACE",
+    "LBRACKET", "RBRACKET",
+    "COMMA", "SEMICOLON",
+    "WSPACE"
 };
 
 class Token{
+public:
     TOKEN_TYPE type;
     std::string value;
+    Token(TOKEN_TYPE t, string v): type(t), value(v) {};
+    friend ostream& operator<<(ostream& os, const Token& token);
 };
+
+ostream& operator<<(ostream& os, const Token& token){
+    os << "<" << token_str[token.type] << ", " << token.value << ">";
+}
 
 #endif
