31eabcb8a1a6a0f3f9897f6666a8ab145d056e6c
ChangminYi <jerry9803@gmail.com>
2021-04-05 18:43:58 +0900

    finish_state_return.txt reallocated

--- compiler-lexical-analyzer/compiler-lexical-analyzer/lexical_analyzer.cpp
+++ compiler-lexical-analyzer/compiler-lexical-analyzer/lexical_analyzer.cpp
@@ -13,8 +13,9 @@ int main(int argc, char *argv[]){
 
 	//initializing of DFA transition table
 	vector<vector<int>> dfa_table = getTransitionTable();
+	vector<int> finish_state_match = getFinishStateReturn();
 
-	vector<string> tokens;	//테스트용 결과 저장용 문자열 배열
+	vector<Token> tokens;	//테스트용 결과 저장용 문자열 배열
 	string temp_input;		//토큰 내용 받는 문자열
 	char in_stream = 0;		//입력 char
 	int current_state = 0;		//start state is 0
@@ -22,7 +23,7 @@ int main(int argc, char *argv[]){
 		int row_togo = get_transit_row(in_stream);	//입력에 맞는 transition table의 col 번호 받아옴
 		
 		if (row_togo == UNAVAILABLE_INPUT_CHAR) {
-			cout << "there is unavailable input: " << in_stream;
+			cerr << "there is unavailable input: " << in_stream;
 			break;
 		}
 		else {
@@ -31,13 +32,18 @@ int main(int argc, char *argv[]){
 			
 			if (new_state == NO_TRANSITION_RULE) {	//기존 state에서 transition rule이 없는 경우
 				//whitespace 예외처리 해야 함
-				tokens.push_back(temp_input);
 
+				if (finish_state_match[current_state] == NO_TRANSITION_RULE) {
+					cerr << "this is not terminal state";
+				}
+				else {
+					tokens.push_back(Token(static_cast<TOKEN_TYPE>(finish_state_match[current_state]), temp_input));
 
-				//현재 input에 맞게 state 다시 설정
-				temp_input.clear();
-				temp_input.push_back(in_stream);
-				current_state = dfa_table.front()[row_togo];
+					//현재 input에 맞게 state 다시 설정
+					temp_input.clear();
+					temp_input.push_back(in_stream);
+					current_state = dfa_table.front()[row_togo];
+				}
 			}
 			else {
 				//문자열에 입력 추가하고 계속 진행
@@ -47,9 +53,9 @@ int main(int argc, char *argv[]){
 		}
 	}
 
-	//테스트 출력
-	for (string &s : tokens) {
-		cout << s << '\n';
+	//테스트
+	for (Token &t : tokens) {
+		cout << t << '\n';
 	}
 
     return 0;
